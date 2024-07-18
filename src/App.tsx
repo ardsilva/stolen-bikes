9a2d623fe7a106c5650be02e9813134753024772
Alexandre Ribeiro da Silva <ardsilva87@gmail.com>
2024-07-18 10:09:57 +0100

    remove Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import Example from "@/components/Example";
-import Component from "@/components/Component";
 
 export default function App() {
 
   return (
     <>
       <Example />
-      <Component />
     </>
   )
 }
